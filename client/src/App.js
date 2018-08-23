e5e2080b3f9238fb1bb142da99871c32456be413
Anssi Hautaviita <anssi.hautaviita@summarum.fi>
2018-08-24 00:05:01 +0300

    Express and React working side by side via proxy

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react'
 import Navbar from './components/Navbar'
+import axios from 'axios'
 import './App.css'
 
 class App extends Component {
+  state = {
+    response: ""
+  }
+
+  componentDidMount() {
+    axios.get("/api/hello")
+      .then(res => {
+        this.setState({ response: res.data.message })
+      })
+      .catch(err => {
+        console.log(err)
+        this.setState({ response: "Tapahtui virhe. Sori siitä" })
+      })
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar />
+        <p>{ this.state.response }</p>
       </div>
     )
   }
