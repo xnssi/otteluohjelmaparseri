c4ecfe5099467a0b2e8ad6b8800d80b0899c9300
Anssi Hautaviita <anssi.hautaviita@summarum.fi>
2018-08-24 00:07:05 +0300

    Heroku deployability

--- server.js
+++ server.js
@@ -9,4 +9,12 @@ app.get('/api/hello', (req, res) => {
     })
 })
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')))
+
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+    })
+}
+
 app.listen(port, () => console.log(`Listening on port ${port}`))
