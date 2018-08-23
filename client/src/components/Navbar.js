4fc4658de709748bb5d41b2e7838b74bcbce459a
Anssi Hautaviita <anssi.hautaviita@summarum.fi>
2018-08-23 23:35:47 +0300

    Move React app to /client

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Navbar extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container">
+                    <span className="navbar-brand" href="/">Otteluohjelmaparseri</span>
+                </div>
+            </nav>
+        )
+    }
+}
+
+export default Navbar
